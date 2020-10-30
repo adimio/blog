e5161979d5ae5729d0d8a59f65b676166967f0c6
鞠文坛 <947876037@qq.com>
2020-10-30 11:35:04 +0800

    添加了关于邮件的轮子（没写）

---
+++ makeWheel/app/Wheel/Email/Email.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Wheel\Email;
+
+
+class Email
+{
+    private $user_email;
+    private $content;
+
+    public function __construct($email)
+    {
+        $this->user_email = $email;
+    }
+
+    public function getEmail(){
+        
+    }
+
+    public function getFile(){
+
+    }
+}
